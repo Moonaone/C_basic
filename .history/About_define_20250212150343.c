abaaf889f982bdcabecdaed51e98dffc50a397cf
Moonaone <2014826196@qq.com>
2025-02-12 15:04:00 +0800

    宏函数的使用

---
+++ .history/About_define_20250212150343.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define LOG_INFO(x) printf("%s is %d\n", #x, (x));
+#define DAY_DELARE(x) int day##x
+#define DAY(x) day##x
+#define ADD(x, y) do{(x = (x) + (y));(x++);} while(0)
+
+int main(void)
+{
+    int a = 10;
+    DAY_DELARE(1) = 100;
+    DAY_DELARE(22) = 200;
+    LOG_INFO(DAY(1));
+    LOG_INFO(DAY(22));
+
+    if(0)
+        ADD(a, 10);
+    else
+        ADD(a, 100);
+
+    printf("a is %d\n", a);
+
+    printf("sizeof = %d \n", sizeof(a));
+    printf("sizeof = %d \n", sizeof a);
+    
+    return 0;
+}
