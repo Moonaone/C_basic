bfb6c13de8b4924d5bfe7779d617b52c1c5d5eb8
Moonaone <2014826196@qq.com>
2025-02-12 22:00:43 +0800

    寄存器操作基础知识

---
+++ .history/Operation_Register_20250212220001.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define CALCULATE_LEN(a) (sizeof(a) * 8)
+
+void print_binary(char a, int n)
+{
+    printf(": ");
+    while(n--)
+    {
+        printf("%d ", (a >> n) & 0x01);
+    }
+    printf(": %d\n", a);
+}
+
+void main()
+{
+    char a = 0x80;
+
+    for(int i = 0; i < 9; i++)
+    {
+        printf("a =  0x%x, right shift %d times", a & 0xFF, i);
+        print_binary((a >> i), CALCULATE_LEN(a));
+    }
+
+    unsigned char b = 0x80;
+
+    for(int i = 0; i < 9; i++)
+    {
+        printf("b =  0x%x, right shift %d times", b & 0xFF, i);
+        print_binary((b >> i), CALCULATE_LEN(b));
+    }
+}
