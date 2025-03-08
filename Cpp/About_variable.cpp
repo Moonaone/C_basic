8546b243757b9f0dff55969e1ff24c35ac93223d
Moonaone <2014826196@qq.com>
2025-03-08 09:51:04 +0800

    关于变量

---
+++ Cpp/About_variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << sizeof(int) << std::endl;
+    std::cout << sizeof(char) << std::endl;
+    std::cout << sizeof(long) << std::endl;
+    std::cout << sizeof(bool) << std::endl;
+    std::cout << sizeof(short) << std::endl;
+    std::cout << sizeof(long long) << std::endl;
+    std::cout << sizeof(float) << std::endl;
+    std::cout << sizeof(double) << std::endl;
+    std::cout << sizeof(unsigned int) << std::endl;
+    std::cout << sizeof(unsigned char) << std::endl;
+
+    std::cin.get();
+}
