9da76027e6e5fe44a97ee6a69fe930bf8bb44840
Moonaone <2014826196@qq.com>
2025-03-08 16:30:08 +0800

    关于类

---
+++ Cpp/About_kind.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl;
+
+class Player
+{
+    public:
+        int x, y;
+        int speed;
+
+    void Move(int xa, int ya)
+    {
+        x += xa * speed;
+        y += ya * speed;
+    }
+};
+
+// void Move(Player &player, int xa, int ya)
+// {
+//     player.x += xa * player.speed;
+//     player.y += ya * player.speed;
+// }
+
+int main()
+{
+    Player player;
+    player.Move(1, -1);
+
+    std::cin.get();
+}
+
