12a1c6d72274e415b934d08a26c65ec372d7245a
Moonaone <2014826196@qq.com>
2025-03-08 17:01:01 +0800

    简单的日志打印

---
+++ .history/Cpp/About_log_20250308170010.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+class Log
+{
+    public:
+    const int LoglevelWarning = 1;
+    const int LoglevelError = 0;
+    const int LoglevelInfo = 2;
+
+    private:
+    int m_Loglevel = LoglevelInfo;
+
+    public:
+    void SetLevel(int level)
+    {
+        m_Loglevel = level;
+    }
+
+    void Warn(const char* message)
+    {
+        if(m_Loglevel >= LoglevelWarning)
+        {
+            std::cout << "[WARNING]:" << message << std::endl;
+        }
+    }
+
+    void Error(const char* message)
+    {
+        if(m_Loglevel >= LoglevelError)
+        {
+            std::cout << "[ERROR]:" << message << std::endl;
+        }
+    }
+
+    void Info(const char* message)
+    {
+        if(m_Loglevel >= LoglevelInfo)
+        {
+            std::cout << "[INFO]:" << message << std::endl;
+        }
+    }
+};
+
+int main()
+{
+    Log log;
+    log.SetLevel(log.LoglevelWarning);
+    log.Warn("Hello!");
+    log.Error("Hello");
+    log.Info("Hello");
+
+    std::cin.get();
+}
