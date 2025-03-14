1aa1dedea9ad6c8017b521a77fb3f8b3c368cfe5
Moonaone <2014826196@qq.com>
2025-03-14 22:35:25 +0800

    关于静态数组

---
+++ Cpp/About_Static_Arrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+#include <array>
+
+void PrintArray(const std::array<int, 5>& data)
+{
+    // for(int i = 0; i < size; i++)
+    // {
+    //     std::cout << array[i] << std::endl;
+    // }
+    for(int i = 0; i < data.size(); i++)
+    {
+        
+    }
+}
+
+int main()
+{
+    std::array<int, 5> data;
+    data[0] = 2;
+    data[4] = 1;
+
+    int dataOld[5];
+    dataOld[0] = 0;
+
+    std::cin.get();
+}
