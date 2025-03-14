5fd8cf859f5c3a14c9cb62a33162d5c212ce6138
Moonaone <2014826196@qq.com>
2025-03-14 19:38:21 +0800

    关于宏定义

---
+++ Cpp/About_define.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+#define WAIT std::cin.get()
+#define LOG(x) std::cout << x << std::endl;
+
+int main()
+{
+    LOG("Hello World");
+
+    // std::cin.get();
+    WAIT;
+}
