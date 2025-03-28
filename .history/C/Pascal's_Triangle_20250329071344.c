1a06dd4c4e3664411451171f078d9a5cb1ff4c7e
Moonaone <2014826196@qq.com>
2025-03-29 07:14:42 +0800

    巴斯卡三角形

---
+++ .history/C/Pascal's_Triangle_20250329071344.c
@@ -0,0 +1,50 @@
+/*
+    °ÍË¹¿¨Èý½ÇÐÎ
+*/
+
+#include <stdio.h>
+
+#define N 6
+
+long combi(int n, int r)
+{
+    int i;
+    long p = 1;
+    for(i = 1; i <= r; i++)
+    {
+        p = p * (n - i  + 1) / i;
+    }
+    return p;
+}
+
+void paint()
+{
+    int n, r, t;
+    for(n = 0; n <= N; n++)
+    {
+        for(r = 0; r <= n; r++)
+        {
+            int i;
+            if(r == 0)
+            {
+                for(i = 0;i <= (N - n); i++)
+                {
+                    printf("    ");
+                }
+            }
+            else
+            {
+                printf("    ");
+            }
+            printf("%3d", combi(n, r));
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    paint();
+
+    return 0;
+}
